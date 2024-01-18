f5c64858a957b4f48d46b02c685dcff985481e9c
Leticia Adao Gomes <100699955+adaogomesl@users.noreply.github.com>
2024-01-18 11:25:18 -0500

    Add files via upload

---
+++ Molcas/Hartree_Fock/example-HF.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+## script for OpenMalCas
+## $INPUT and $WORKDIR do not belong to OpenMolCas
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=23:00:00
+#SBATCH --job-name=bf3-tropone-hf
+#SBATCH --partition=lopez
+#SBATCH --mem=4Gb
+#SBATCH --output=%j.o.slurm
+#SBATCH --error=%j.e.slurm
+
+export INPUT=bf3-tropone
+export WORKDIR=$PWD
+
+export MOLCAS_NPROCS=$SLURM_NTASKS
+export MOLCAS_MEM=2870
+export MOLCAS_PRINT=3
+export MOLCAS_PROJECT=$INPUT
+export MOLCAS_WORKDIR=$WORKDIR
+export OMP_NUM_THREADS=1
+export MOLCAS=/work/lopez/Molcas
+
+module load python/3.7.1
+
+cd $WORKDIR
+$MOLCAS/bin/pymolcas -f $INPUT.inp -b 1
+rm -r $MOLCAS_PROJECT
